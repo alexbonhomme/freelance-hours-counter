4f67dc86d0bda886630d0d6acc7e5f654aa11815
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2020-09-03 21:12:09 +0200

    feat: impl day counter

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,25 +10,51 @@
     </md-app-toolbar>
 
     <md-app-content>
-      <div v-for="event in events" v-bind:key="event.id">
-        {{ event }}
+      <!-- token query -->
+      <md-field>
+        <label>Token</label>
+        <md-input
+          v-model="token"
+          @keypress.enter="fetchEvents()"
+        ></md-input>
+      </md-field>
+
+      <!-- event list -->
+      <md-table>
+        <md-table-row>
+          <md-table-head md-numeric>ID</md-table-head>
+          <md-table-head>Summary</md-table-head>
+          <md-table-head>Duration (minutes)</md-table-head>
+          <md-table-head>Created</md-table-head>
+        </md-table-row>
+
+        <md-table-row v-for="event in events" v-bind:key="event.id">
+          <md-table-cell md-numeric>{{ event.id }}</md-table-cell>
+          <md-table-cell>{{ event.summary }}</md-table-cell>
+          <md-table-cell>{{ event.durationInMinutes }}</md-table-cell>
+          <md-table-cell>{{ event.created }}</md-table-cell>
+        </md-table-row>
+      </md-table>
+
+      <!-- hours counter -->
+      <div>
+        <span class="md-body-2">{{ dayCount.toFixed(2) }} day(s)</span>
       </div>
     </md-app-content>
   </md-app>
 </template>
 
 <script>
+const HOURS_PER_DAY = 7;
+
 export default {
   name: 'Home',
   data() {
     return {
-      events: undefined,
-      gapi: undefined,
-      isAuthenticated: false,
+      token: '',
+      events: [],
+      dayCount: 0
     }
-  },
-  created() {
-    this.listUpcomingEvents()
   },
   methods: {
     /**
@@ -45,17 +71,34 @@ export default {
      * the authorized user's calendar. If no events are found an
      * appropriate message is printed.
      */
-    listUpcomingEvents() {
+    fetchEvents() {
       this.$gapi.getGapiClient().then(gapi => {
         gapi.client.calendar.events.list({
           'calendarId': 'primary',
-          'timeMin': (new Date()).toISOString(),
+          'timeMin': (new Date('2020-08-01')).toISOString(),
+          'timeMax': (new Date()).toISOString(),
           'showDeleted': false,
           'singleEvents': true,
-          'maxResults': 10,
-          'orderBy': 'startTime'
+          // 'maxResults': 10,
+          'orderBy': 'startTime',
+          'q': this.token
         }).then((response) => {
-          this.events = response.result.items
+          this.events = response.result.items.filter(item => item.summary.indexOf(this.token) === 0)
+
+          this.events = this.events.map(event => {
+            const start = new Date(event.start.dateTime)
+            const end = new Date(event.end.dateTime)
+
+            event.durationInMinutes = (end.getTime() - start.getTime()) / (1000 * 60);
+
+            return event
+          })
+
+          const hourCout = this.events.reduce((counter, event) => {
+            return counter + event.durationInMinutes
+          }, 0)
+
+          this.dayCount = hourCout / (60 * HOURS_PER_DAY)
         });
       })
     }
