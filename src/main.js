cd54b4f5d01468477f194b065a3beb6e77fc726c
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2020-09-03 18:48:15 +0200

    refactor(gapi): use vue-gapi dep

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// import the plugin
+import VueGAPI from 'vue-gapi'
+
+Vue.config.productionTip = false
+
+// create the 'options' object
+const apiConfig = {
+  apiKey: 'AIzaSyCS93f7DH1wgsZ6xtl28t9EiEVRCp3LHh8',
+  clientId: '716599607292-rvhmhiq4jth1orikggk0ihjhnvh4cmcf.apps.googleusercontent.com',
+  discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+  // see all available scopes here: https://developers.google.com/identity/protocols/googlescopes'
+  scope: 'https://www.googleapis.com/auth/calendar.readonly',
+
+  // works only with `ux_mode: "prompt"`
+  refreshToken: true,
+}
+
+// Use the plugin and pass along the configuration
+Vue.use(VueGAPI, apiConfig)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
